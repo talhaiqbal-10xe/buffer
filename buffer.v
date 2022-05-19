3751125ce9c1a472d9d6110c8a2e5bb35e04d708
talhaiqbal-10xe <102578954+talhaiqbal-10xe@users.noreply.github.com>
2022-05-19 15:59:46 +0500

    buffer updated Sobel Applied

--- buffer.v
+++ buffer.v
@@ -18,27 +18,52 @@
 // Additional Comments: 
 //
 //////////////////////////////////////////////////////////////////////////////////
-module buffer(
+module buffer
+#(parameter DataBitWidth=4, parameter ExtraBits=3,
+  parameter Channels=3)
+(
 input clk,rst,en,
-input [11:0] d_in,
-output [11:0] d_out
+input [DataBitWidth*Channels-1:0] d_in,
+output [DataBitWidth*Channels-1:0] d_out
     );
-reg [11:0] mem [8:0];
 integer i;
-assign d_out=mem[8]+mem[7]+mem[6]+mem[5]+mem[4]+mem[3]+mem[2]+mem[1]+mem[0];
+reg signed [DataBitWidth+ExtraBits-1:0] mem1 [8:0];
+reg signed [DataBitWidth+ExtraBits-1:0] mem2 [8:0];
+reg signed [DataBitWidth+ExtraBits-1:0] mem3 [8:0];
+
+wire signed [DataBitWidth+ExtraBits-1:0] out1,out2,out3;
+
+assign out1=mem1[8]*-1+mem1[7]*-2+mem1[6]*-1+mem1[2]+mem1[1]*2+mem1[0];
+assign out2=mem2[8]*-1+mem2[7]*-2+mem2[6]*-1+mem2[2]+mem2[1]*2+mem2[0];
+assign out3=mem3[8]*-1+mem3[7]*-2+mem3[6]*-1+mem3[2]+mem3[1]*2+mem3[0];
+
+assign d_out[DataBitWidth-1:0]= out1<0 ? 0: out1>15 ? 15: out1[DataBitWidth-1:0];
+assign d_out[2*DataBitWidth-1:DataBitWidth]= out2<0 ? 0: out2>15 ? 15: out2[DataBitWidth-1:0];
+assign d_out[3*DataBitWidth-1:2*DataBitWidth]= out3<0 ? 0: out3>15 ? 15: out3[DataBitWidth-1:0];
 
 always @(posedge clk)
 if (rst)
     begin
 	 for ( i=0;i<=8;i=i+1)
-         mem[i]<=0;
+         begin
+			mem1[i]<=0;
+			mem2[i]<=0;
+			mem3[i]<=0;
+			end
 	 end
 else 
     if(en)
        begin
-       mem[8]<=d_in;
+       mem1[8][DataBitWidth-1:0]<=d_in[DataBitWidth-1:0];
+		 mem2[8][DataBitWidth-1:0]<=d_in[2*DataBitWidth-1:DataBitWidth];
+		 mem3[8][DataBitWidth-1:0]<=d_in[3*DataBitWidth-1:2*DataBitWidth];
 		 for ( i=0;i<=7;i=i+1)
-            mem[i]<=mem[i+1];
+            begin
+				mem1[i][DataBitWidth-1:0]<=mem1[i+1];
+				mem2[i][DataBitWidth-1:0]<=mem2[i+1];
+				mem3[i][DataBitWidth-1:0]<=mem3[i+1];
+				end
+				
 					
 	    end
 
